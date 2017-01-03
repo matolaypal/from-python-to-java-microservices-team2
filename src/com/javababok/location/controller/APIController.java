ba3295ff0634a6897b4ed84455c2c68d06181bea
BTamara <bolyatami@gmail.com>
2017-01-03 10:39:02 +0100

    merge and correction with APIService

--- src/com/javababok/location/controller/APIController.java
+++ src/com/javababok/location/controller/APIController.java
@@ -1,20 +1,26 @@
 package com.javababok.location.controller;
+import com.javababok.location.service.APIService;
+import org.json.JSONObject;
 import spark.Request;
 import spark.Response;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
 
 
 public class APIController {
 
     private final APIService apiService;
+    private static final String origin  = "Budapest";
 
     public APIController(APIService apiService){
 
         this.apiService = apiService;
     }
 
-    public String location (Request request, Response response){
+    public JSONObject location (Request request, Response response) throws IOException, URISyntaxException {
 
-        return apiService.calcTime(request.queryParams("destination"));
+        return apiService.calcTime(origin, request.queryParams("destination"));
     }
 
 
